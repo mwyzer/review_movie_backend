37f9915051c0c060f9cae4538ec4bd411e377257
mwyzer <muhammad.wyzer@gmail.com>
2023-03-22 18:34:10 +0700

    async await error handling

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+require('express-async-errors');
+require('dotenv').config();
+require('./db');
+const userRouter = require('./routes/user');
+const { errorHandler } = require('./middleware/error');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api/user', userRouter);
+
+app.use(errorHandler);
+
+// app.post(
+//   '/sign-in',
+//   (req, res, next) => {
+//     const { email, password } = req.body;
+//     if (!email || !password)
+//       return res.json({ error: 'email or password are missing!' });
+//     next();
+//   },
+//   (req, res) => {
+//     res.send('<h1>Hello I am from</h1>');
+//   }
+// );
+
+app.listen(8000, () => {
+  console.log('the port is listening on port 8000');
+});
